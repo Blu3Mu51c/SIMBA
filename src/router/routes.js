279138f0ad859cee2c407323f4a2ab98332f1fcc
Hamza <hamzaiqbal146@gmail.com>
2025-08-25 16:00:21 +0300

    i'm sowwy i did a lot oppsies

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,7 +1,5 @@
-// routes.js
 import AnalyticsPage from '../pages/Analytics/AnalyticsPage/AnalyticsPage';
 import ProfilePage from '../pages/Profile/ProfilePage/ProfilePage';
-
 // User Pages
 import ItemsPage from '../pages/Items/ItemsPage/ItemsPage';
 import ItemsEditPage from '../pages/Items/ItemsEditPage/ItemsEditPage';
@@ -9,17 +7,15 @@ import ItemsShowPage from '../pages/Items/ItemsShowPage/Items';
 import OrdersPage from '../pages/Booking/OrdersPage/OrdersPage';
 import CartPage from '../pages/Booking/CartPage/CartPage';
 import StudentRequestsPage from '../pages/Booking/StudentRequestsPage/StudentRequestsPage';
-
 // Admin Pages
 import StockRequestPage from '../pages/Management/StockRequest/StockRequestPage';
-
 export const adminRoutes = [
   { path: '/analytics', element: <AnalyticsPage /> },
   { path: '/profile', element: <ProfilePage /> },
   { path: '/stock-request', element: <StockRequestPage /> },
 ];
-
 export const userRoutes = [
+  { path: '/analytics', element: <AnalyticsPage /> },
   { path: '/profile', element: <ProfilePage /> },
   { path: '/items', element: <ItemsPage /> },
   { path: '/items/edit/:id', element: <ItemsEditPage /> },
